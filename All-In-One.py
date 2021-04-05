54eb0a13b09d42c8ed587bac25e1f789d5a23c33
ayman-elkassas <aymanelkassas88@gmail.com>
2021-04-05 21:52:11 +0200

    python all in one

---
+++ All-In-One.py
@@ -0,0 +1,12 @@
+# todo:variable and strings
+print("Hello world")
+# with var
+msg = "hello world"
+print(msg)
+
+# todo:concatenation
+first_name = "ahmed"
+last_name = "mohamed"
+print(first_name+''+last_name)
+
+# todo:list
